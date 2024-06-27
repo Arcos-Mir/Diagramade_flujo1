494eb477e52262061ce5836af1db01a416c4d30f
Mir Arcos <arc.mir24@gmail.com>
2024-06-26 22:33:28 -0600

    first commit diagramas de flujo e1

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Ejercicio 1 hacer un programa que permita la suma de 2 variables de tipo entero */
+
+//Definir variables
+
+let number1 = 0;
+let number2 = 0;
+let result = 0;
+
+//ingresar mediante un prompt 2 números y guardarlos en las variables
+
+number1 = prompt("ingresa un número entero");
+number2 = prompt("ingresa otro número entero");
+
+console.log(number1);
+console.log(number2);
+
+//realizar la operación solicitada en el problema(suma)
+
+result = parseInt(number1) + parseInt(number2);
+
+//mostrar por consola el resultado de la operación
+
+console.log(result);
+
+
